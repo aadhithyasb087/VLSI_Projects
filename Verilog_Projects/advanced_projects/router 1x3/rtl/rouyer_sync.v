5b7b517518e7b0f197e3319c8d81dae286db2925
aadhithyasb087 <152068655+aadhithyasb087@users.noreply.github.com>
2025-06-03 09:12:02 +0530

    Update router_sync.v

--- Verilog_Projects/advanced_projects/router 1x3/rtl/rouyer_sync.v
+++ Verilog_Projects/advanced_projects/router 1x3/rtl/rouyer_sync.v
@@ -1,58 +1,83 @@
-// Synchronizer: Handles control signal coordination and soft reset generation
+module router_sync( detect_add,write_enb_reg,clock,resetn,read_enb_0,read_enb_1,read_enb_2,empty_0,empty_1,empty_2,full_0,full_1,full_2,data_in,vld_out_0,vld_out_1,vld_out_2,fifo_full,soft_reset_0,soft_reset_1,soft_reset_2,write_enb);
 
-module router_sync(
-    input wire clk,
-    input wire rstn,
-    input wire full_0, full_1, full_2,
-    input wire empty_0, empty_1, empty_2,
-    input wire detect_add,
-    input wire [1:0] data_in,
-    input wire write_enb_reg,
-    input wire read_enb_0, read_enb_1, read_enb_2,
-    output reg [2:0] write_enb,
-    output reg fifo_full,
-    output reg vld_out_0, vld_out_1, vld_out_2,
-    output reg soft_reset_0, soft_reset_1, soft_reset_2
-);
+  input detect_add,write_enb_reg,clock,resetn,read_enb_0,read_enb_1,read_enb_2,empty_0,empty_1,empty_2,full_0,full_1,full_2;
+  input [1:0]data_in;
+  output vld_out_0,vld_out_1,vld_out_2;
+  output reg fifo_full,soft_reset_0,soft_reset_1,soft_reset_2;
+  output reg [2:0]write_enb;
+  reg [1:0]temp;
+  reg [4:0]count0;
+  reg [4:0]count1;
+  reg [4:0]count2;
 
-    always @(*) begin
-        // Default assignments
-        write_enb = 3'b000;
-        fifo_full = 0;
-        vld_out_0 = 0;
-        vld_out_1 = 0;
-        vld_out_2 = 0;
-        soft_reset_0 = 0;
-        soft_reset_1 = 0;
-        soft_reset_2 = 0;
+  assign vld_out_0 = ~empty_0;
+  assign vld_out_1 = ~empty_1;
+  assign vld_out_2 = ~empty_2;
 
-        if (detect_add) begin
-            case(data_in)
-                2'b00: begin
-                    fifo_full = full_0;
-                    write_enb[0] = write_enb_reg;
-                end
-                2'b01: begin
-                    fifo_full = full_1;
-                    write_enb[1] = write_enb_reg;
-                end
-                2'b10: begin
-                    fifo_full = full_2;
-                    write_enb[2] = write_enb_reg;
-                end
-                default: fifo_full = 0;
-            endcase
-        end
+  always@(posedge clock)begin
+    if(!resetn)
+      temp <=2'b11;
+    else if(detect_add)
+      temp <= data_in;
+  end
 
-        // Valid output flags based on empty signals
-        if (!empty_0) vld_out_0 = 1;
-        if (!empty_1) vld_out_1 = 1;
-        if (!empty_2) vld_out_2 = 1;
-
-        // Soft resets based on simultaneous read+empty
-        if (read_enb_0 && empty_0) soft_reset_0 = 1;
-        if (read_enb_1 && empty_1) soft_reset_1 = 1;
-        if (read_enb_2 && empty_2) soft_reset_2 = 1;
+  always@(*)begin
+    case(temp)
+      2'b00:fifo_full<=full_0;
+      2'b01:fifo_full<=full_1;
+      2'b10:fifo_full<=full_2;
+      default:fifo_full<=1'b0;
+   endcase  end
+  always@(*)begin
+    if(write_enb_reg)begin      case(temp)
+        2'b00:write_enb<=3'b001;        2'b01:write_enb<=3'b010;
+        2'b10:write_enb<=3'b100;        default:write_enb<=3'b000;
+      endcase    end
+    else      write_enb=3'b000;
+  end
+  always@(posedge clock)begin    if(!resetn)
+           count0=5'b0;         else if(vld_out_0)begin
+      if(!read_enb_0)begin        if(count0==5'd29)begin
+          soft_reset_0<=1'b1;          count0<=5'b0;
+        end        else begin
+          count0<=count0+5'b1;          soft_reset_0<=1'b0;
+        end      end
+       else
+        count0=5'b0;         end
+    else      count0<=5'b0;
     end
+
+  always@(posedge clock)begin 
+       	  if(!resetn)
+           count1=5'b0; 
+   else if(vld_out_1)begin
+      if(!read_enb_1)begin  
+	      if(count1==5'd29)begin
+          soft_reset_1<=1'b1;   
+   	  count1<=5'b0;
+        end   
+   	else begin
+          count1<=count1+5'b1;    
+    	  soft_reset_1<=1'b0;
+        end    
+end
+                else   
+	 		count1=5'b0;
+         end  
+       	 else
+      count1<=5'b0; 
+   end
+
+always@(posedge clock)begin
+    if(!resetn)           count2=5'b0;
+         else if(vld_out_2)begin
+if(!read_enb_2)begin        if(count2==5'd29)begin
+          soft_reset_2<=1'b1;          count2<=5'b0;
+        end        else begin
+          count2<=count2+5'b1;          soft_reset_2<=1'b0;
+        end      end
+                else                  count2=5'b0;
+         end    else
+      count2<=5'b0;    end
 
 endmodule
